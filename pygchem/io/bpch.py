cb1624280cde792dcc6c87b40d2e909282077c97
Daniel Rothenberg <darothen@mit.edu>
2017-01-31 00:16:38 -0500

    Fixing leftover reference to file_position

--- pygchem/io/bpch.py
+++ pygchem/io/bpch.py
@@ -89,7 +89,7 @@ class BPCHDataProxy(object):
     def __repr__(self):
         fmt = '<{self.__class__.__name__} shape={self.shape}' \
               ' dtype={self.dtype!r} path={self.path!r}' \
-              ' file_position={self.file_position}' \
+              ' file_positions={self.file_positions}' \
               ' scale_factor={self.scale_factor}>'
         return fmt.format(self=self)
 
